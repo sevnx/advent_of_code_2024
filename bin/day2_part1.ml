b94fd30ad0f9429c95994e6d093bbd35f1555582
Seweryn Czykinowski <sevveryn.czykinowski@gmail.com>
2024-12-02 16:43:39 +0100

    Day 2: Part 1

---
+++ bin/day2_part1.ml
@@ -0,0 +1,47 @@
+let file = "inputs/day2.txt"
+
+type order = Ascending | Descending
+
+let get_ordering first second =
+  match first with
+  | Some first -> if first > second then Some Descending else Some Ascending
+  | None -> None
+
+let diff_abs a b = abs (a - b)
+
+let is_diff_incorrect a b =
+  let diff = diff_abs a b in
+  if diff > 3 || diff == 0 then true else false
+
+let is_line_safe line =
+  let numbers = List.map int_of_string (String.split_on_char ' ' line) in
+  let rec loop last_number rest ascending_type =
+    match rest with
+    | [] -> true
+    | first :: rest -> (
+        let ordering = get_ordering last_number first in
+        let ascending_type =
+          match ascending_type with
+          | Some Ascending -> Some Ascending
+          | Some Descending -> Some Descending
+          | None -> if last_number = None then None else ordering
+        in
+        match ascending_type with
+        | Some value ->
+            if Option.get ordering <> value then false
+            else if is_diff_incorrect (Option.get last_number) first then false
+            else loop (Some first) rest ascending_type
+        | None -> loop (Some first) rest ascending_type)
+  in
+  loop None numbers None
+
+let () =
+  let ic = open_in file in
+
+  let rec aux channel acc =
+    match input_line channel with
+    | line -> aux channel (acc + if is_line_safe line then 1 else 0)
+    | exception End_of_file -> acc
+  in
+  let sum = aux ic 0 in
+  Printf.printf "%d\n" sum
